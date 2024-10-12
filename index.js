48070f849d06e226f0ae43e0c0bd25ba955b27b6
santy0925 <santiagocardona0925@gmail.com>
2024-10-11 20:25:58 -0500

    java scrip

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ const pi = 3.1416;
 // Number
 let entero = 10;
 let decimal = 3.1416;
+
+// String
+
+let Pregunta = "¿Qué hora es?";
+let Respuesta = "Son las 4";
+
+// Boolean
+let EsMayorDeEdad = true;
+let TienePermiso = false;
