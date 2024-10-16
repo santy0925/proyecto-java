299192b741e314152936d5d333fc1a173f791886
santy0925 <santiagocardona0925@gmail.com>
2024-10-16 18:57:38 -0500

    tipos de variables

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const pi = 3.1416;
 // Tipos de Datos
 
 // Number
-let entero = 10;          
+let entero = 10;
 let decimal = 3.1416;
 
 // String
@@ -19,7 +19,26 @@ let TienePermiso = false;
 
 // undefined
 let valor;
-console.log(valor);//underfined
+console.log(valor); //underfined
 
 // null
 let nota = null;
+
+// Tipos  de datos estructurados
+
+//Objects
+let persona = {
+  Nombre: "Filimeno",
+  Edad: 60,
+  esTrabajador: true,
+  Habilidades: {
+    Produccion: 5.0,
+    Empaque: 3.0,
+  },
+};
+
+//camel case = Forma de nombrar nustras variables
+
+// Array Arreglos
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let Frutas = ["Manzana", "Naranja", "Pera"];
